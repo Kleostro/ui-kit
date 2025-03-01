67b7eece9305c7cb5be25c8646b0821c3074ce6e
Max <133934232+Kleostro@users.noreply.github.com>
2025-03-01 21:32:41 +0300

    feat: implement inputText module

---
+++ ui-kit/projects/rss-ui-kit/inputtext/public-api.ts
@@ -0,0 +1,2 @@
+export * from './src/directives/inputtext/inputtext.directive';
+export * from './src/inputtext.module';
---
+++ ui-kit/projects/rss-ui-kit/inputtext/src/inputtext.type.ts
@@ -0,0 +1,7 @@
+export const INPUT_TEXT_VARIANT = {
+  OUTLINED: 'outlined',
+  FILLED: 'filled',
+} as const;
+
+export type InputTextVariantType =
+  (typeof INPUT_TEXT_VARIANT)[keyof typeof INPUT_TEXT_VARIANT];
